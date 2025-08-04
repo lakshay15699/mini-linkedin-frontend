c7793b32ccf1f79a611d36fdc76d52ddd2b25f76
lakshay15699 <blakshay66@gmail.com>
2025-08-05 01:10:18 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
-const instance = axios.create({
-  baseURL: 'https://mini-linkedin-backend-wrzl.onrender.com/api',
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL, 
+  withCredentials: true,                 
 });
 
-export default instance;
+export default api;
